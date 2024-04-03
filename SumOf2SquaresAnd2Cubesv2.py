dbae941f53d526d97633414bdcc53e4ebd038d92
MarkPinsker <107402485+MarkPinsker@users.noreply.github.com>
2024-04-03 17:26:37 +0100

    Add files via upload

--- SumOf2SquaresAnd2Cubesv2.py
+++ SumOf2SquaresAnd2Cubesv2.py
@@ -37,36 +37,6 @@ for i in range(squareShell):
     if ( sumOf2CubesandSquare[i] == 0):
         NotSumOf1Squaresand2CubesNo = NotSumOf1Squaresand2CubesNo + 1
         NotSumOf1Squaresand2Cubes.append(i)
-##############################################################################
-# Calculate all sums of two cubes modulo 63 ( should be 25 of them )
-##############################################################################
-sumOfCubesMod63 = [[0 for i in range(64)] for j in range(64)]
-zMod63Generator = [[[-1 for i in range(1001)] for j in range(64)] for k in range(64)]
-
-base63=63
-for x in range(base63):
-	x3 = (x ** 3) % base63
-
-	for y in range(base63):
-
-		y3 = ( x3 + ( y ** 3 ) ) % base63 
-# this relation zRa is true if z cubed plus some number cubed can sum to a mod 63
-# For example sumOfCubesMod63[2][7] =1 becasue 2^3 + 5^3 = 7 (mod 63)
-		sumOfCubesMod63[y][y3]=1
-
-
-for nMinusxsqmMod63 in range(base63):
-	for y in range(base63):
-		a = ( nMinusxsqmMod63 - (y * y) ) % 63
-		n = 0
-		zMod63Generator[nMinusxsqmMod63][y][0] = n
-		for z in range(1000):
-			zmod63 = z % base63
-			if ( sumOfCubesMod63[zmod63][a] == 1 ):
-				n = n + 1
-
-				zMod63Generator[nMinusxsqmMod63][y][0] = n
-				zMod63Generator[nMinusxsqmMod63][y][n] = z
 
 sumOf2cubes = [0 for i in range(zmaxCubed)]
 for x in range(zMax):
@@ -108,11 +78,8 @@ for xfloor in range(xfloorstart,xfloorend):
 		if ( nMinusxsqm == 0 ):
 			notFound = False
 			#print(n,' = ',x,'^2')
-		nMinusxsqmMod63 = nMinusxsqm % base63
 		while x >= 0 and nMinusxsqm  >=0 and notFound:
 			y = math.floor(math.sqrt(nMinusxsqm) )
-			ymod63 = y % base63
-			noSumof2cubesin63 = zMod63Generator[nMinusxsqmMod63][ymod63][0]
 			nMinusx2Minusy2 = nMinusxsqm - y * y
 # Loop downwards through possible y values if there are any
 			while ( ( y > 0 ) and ( nMinusx2Minusy2  >= 0 ) and notFound):
@@ -120,13 +87,10 @@ for xfloor in range(xfloorstart,xfloorend):
 					notFound = False
 #					print('n= ',n,',x=',x,',y= ',y,',z,w =',sumOf2cubes[nMinusx2Minusy2] )
 				y = y - 1
-				ymod63 = y % base63
-				noSumof2cubesin63 = zMod63Generator[nMinusxsqmMod63][ymod63][0]
 				nMinusx2Minusy2 = nMinusxsqm - y * y
 # Move to next smaller x
 			x = x - 1
 			nMinusxsqm = n - x * x
-			nMinusxsqmMod63 = nMinusxsqm % base63
 		if ( notFound ):
 			print(n,' is not x^2 + y^2 + z^3 + w^3')
 			f.write(str(n) + ' is not x^2 + y^2 + z^3 + w^3\n')
